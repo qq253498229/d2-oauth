1f8024c7d0dc381090f28a90f42f1ab0cedef9f3
王斌 <253498229@qq.com>
2019-03-28 02:30:40 +0800

    add init data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert ignore into `t_client`
+(`id`, `client_id`, `client_secret`,
+ `authorized_grant_types`, `scope`,
+ `refresh_token_validity_seconds`, `access_token_validity_seconds`,
+ `registered_redirect_uri`)
+values (1, 'client', '$2a$10$YACEb/LqIN2eZRSd2sKbuOleAsCm7PIJQ0ob73FKxGrD3dmeGUZPy',
+        'password,authorization_code,refresh_token', 'app',
+        604800, 7200, null);
